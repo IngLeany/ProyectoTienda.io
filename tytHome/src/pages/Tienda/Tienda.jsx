d694035ceb83d7e240848a8f912a52fc69834620
IngLeany <leany2902@gmail.com>
2024-07-19 16:38:00 -0500

    Update Tienda.jsx

--- tytHome/src/pages/Tienda/Tienda.jsx
+++ tytHome/src/pages/Tienda/Tienda.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { FooterHome } from "../../components/FooterHome/FooterHome";
+import vestidodebaño1 from "../../assets/images/vestidodebaño1.png";
+
 import "./Tienda.css";
 
 
@@ -9,8 +11,79 @@ export const Tienda = () => {
     <>
       <Navbar />
 
+<div className="producto">
+    <img src={vestidodebaño1} alt="Producto"/>
+    <div className="producto-info">
+        <h2>Nombre del Producto</h2>
+        <p className="precio">Precio (COP): $100.000</p>
+        <p className="precio">Precio (USD): $30</p>
+    </div>
+</div>
+<div className="producto">
+    <img src="producto.jpg" alt="Producto"/>
+    <div className="producto-info">
+        <h2>Nombre del Producto</h2>
+        <p className="precio">Precio (COP): $100.000</p>
+        <p className="precio">Precio (USD): $30</p>
+    </div>
+</div>
+<div className="producto">
+    <img src="producto.jpg" alt="Producto"/>
+    <div className="producto-info">
+        <h2>Nombre del Producto</h2>
+        <p className="precio">Precio (COP): $100.000</p>
+        <p className="precio">Precio (USD): $30</p>
+    </div>
+</div>
+<div className="producto">
+    <img src="producto.jpg" alt="Producto"/>
+    <div className="producto-info">
+        <h2>Nombre del Producto</h2>
+        <p className="precio">Precio (COP): $100.000</p>
+        <p className="precio">Precio (USD): $30</p>
+    </div>
+</div>
+<div className="producto">
+    <img src="producto.jpg" alt="Producto"/>
+    <div className="producto-info">
+        <h2>Nombre del Producto</h2>
+        <p className="precio">Precio (COP): $100.000</p>
+        <p className="precio">Precio (USD): $30</p>
+    </div>
+</div>
+<div className="producto">
+    <img src="producto.jpg" alt="Producto"/>
+    <div className="producto-info">
+        <h2>Nombre del Producto</h2>
+        <p className="precio">Precio (COP): $100.000</p>
+        <p className="precio">Precio (USD): $30</p>
+    </div>
+</div>
+<div className="producto">
+    <img src="producto.jpg" alt="Producto"/>
+    <div className="producto-info">
+        <h2>Nombre del Producto</h2>
+        <p className="precio">Precio (COP): $100.000</p>
+        <p className="precio">Precio (USD): $30</p>
+    </div>
+</div>
+<div className="producto">
+    <img src="producto.jpg" alt="Producto"/>
+    <div className="producto-info">
+        <h2>Nombre del Producto</h2>
+        <p className="precio">Precio (COP): $100.000</p>
+        <p className="precio">Precio (USD): $30</p>
+    </div>
+</div>
+<div className="producto">
+    <img src="producto.jpg" alt="Producto"/>
+    <div className="producto-info">
+        <h2>Nombre del Producto</h2>
+        <p className="precio">Precio (COP): $100.000</p>
+        <p className="precio">Precio (USD): $30</p>
+    </div>
+</div>
       <FooterHome />
     </>
   );
 };
-

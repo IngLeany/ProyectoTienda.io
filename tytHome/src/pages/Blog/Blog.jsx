3bda482be58a39eb552331da6f8346c625979acb
IngLeany <leany2902@gmail.com>
2024-07-24 17:15:37 -0500

    Update Blog.jsx

--- tytHome/src/pages/Blog/Blog.jsx
+++ tytHome/src/pages/Blog/Blog.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { FooterHome } from "../../components/FooterHome/FooterHome";
 import "./Blog.css";
-import Videotythome from "../../assets/videos/Videotythome.mp4";;
+import bannerblog from "../../assets/images/bannerblog.png";
 import cloto from "../../assets/images/cloto.png";
 import ika from "../../assets/images/ika.png";
 import rebeca from "../../assets/images/rebeca.png";
@@ -18,7 +18,7 @@ export const Blog = () => {
           <div className="container">
             <div className="row">
               <div className="col-md-12 mb-0">
-                <a href="">Inicio</a>
+                <a href="/">Inicio</a>
                 <span className="mx-2 mb-0">/</span>
                 <strong className="text-black">Blog</strong>
               </div>
@@ -31,107 +31,39 @@ export const Blog = () => {
             <div className="row mb-5">
               <div className="col-md-6">
                 <div className="block-16">
-                  <figure>
-                  <video autoPlay controls>
-                  <source src={Videotythome} type="video/mp4" />
-                   </video>
-                  </figure>
-                </div>
-              </div>
-              <div className="col-md-1"></div>
-              <div className="col-md-5">
-                <div className="site-section-heading pt-3 mb-4">
-                  <h2 className="text-black">BLOG</h2>
-                </div>
-                <p>
-                  Somos una tienda de productos elaborados con materias primas
-                  ecológicas y orgánicas, mediante procesos sostenibles.
-                </p>
-                <p>TRABAJAMOS BAJO 4 PILARES</p>
-                <p>Sostenibilidad social.</p>
-                <p>Productos realizados de mujer a mujer.</p>
-                <p>Sostenibilidad medioambiental.</p>
-                <p>Somos un estilo de vida.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="site-section border-bottom" data-aos="fade">
-          <div className="container">
-            <div className="row justify-content-center mb-5">
-              <div className="col-md-7 site-section-heading text-center pt-4">
-                <h2>NUESTRAS MARCAS</h2>
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col-md-6 col-lg-3">
-                <div className="block-38 text-center">
-                  <div className="block-38-img">
-                    <div className="block-38-header">
-                      <img src={cloto} alt="Image placeholder" className="mb-4" />
-                      <h3 className="block-38-heading h4">MARCA</h3>
-                      <p className="block-38-subheading">CLOTO</p>
+                  
+                    <div className="col-md-5">
+                      <div className="site-section-heading pt-3 mb-4">
+                        <h2 className="text-black">BLOG</h2>
+                      </div>
+                      <p>
+                        Somos una tienda de productos elaborados con materias
+                        primas ecológicas y orgánicas, mediante procesos
+                        sostenibles.
+                      </p>
+                      <p>TRABAJAMOS BAJO 4 PILARES</p>
+                      <p>1. Sostenibilidad social.</p>
+                      <p>2. Productos realizados de mujer a mujer.</p>
+                      <p>3. Sostenibilidad medioambiental.</p>
+                      <p>4. Somos un estilo de vida.</p>
                     </div>
-                    <div className="block-38-body">
-                      <p>En nuestra marca de cloto encontrarás la línea de pijamas y ropa de descanso.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-md-6 col-lg-3">
-                <div className="block-38 text-center">
-                  <div className="block-38-img">
-                    <div className="block-38-header">
-                      <img src={ika} alt="Image placeholder" className="mb-4" />
-                      <h3 className="block-38-heading h4">MARCA</h3>
-                      <p className="block-38-subheading">IKA</p>
-                    </div>
-                    <div className="block-38-body">
-                      <p>En nuestra marca IKA encontrarás la línea de vestidos de baño, lencería para el hogar, sandalias y mucho más.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-md-6 col-lg-3">
-                <div className="block-38 text-center">
-                  <div className="block-38-img">
-                    <div className="block-38-header">
-                      <img src={rebeca} alt="Image placeholder" className="mb-4" />
-                      <h3 className="block-38-heading h4">MARCA</h3>
-                      <p className="block-38-subheading">REBECA</p>
-                    </div>
-                    <div className="block-38-body">
-                      <p>En nuestra marca Rebeca encontrarás ropa de lino y mucho más.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-md-6 col-lg-3">
-                <div className="block-38 text-center">
-                  <div className="block-38-img">
-                    <div className="block-38-header">
-                      <img src={tythome} alt="Image placeholder" className="mb-4" />
-                      <h3 className="block-38-heading h4">TYT</h3>
-                      <p className="block-38-subheading">HOME</p>
-                    </div>
-                    <div className="block-38-body">
-                      <p>Conoce nuestra marca de productos 100% orgánicos & ecológicos...</p>
-                    </div>
-                  </div>
+                    <img
+                      src={bannerblog}
+                      alt="Image placeholder"
+                      className="mb-4"
+                      
+                    />
+                 
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+        </div>
+      
+     
 
       <FooterHome />
     </>
   );
 };
-

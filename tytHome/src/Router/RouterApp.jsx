1766b01ee04155360c41843213c080fbc241b513
IngLeany <leany2902@gmail.com>
2024-07-26 14:14:23 -0500

    Update RouterApp.jsx

--- tytHome/src/Router/RouterApp.jsx
+++ tytHome/src/Router/RouterApp.jsx
@@ -2,9 +2,9 @@ import { createBrowserRouter } from 'react-router-dom';
 import { Layout } from '../components/Layout/Layout';
 import { DetalleHome } from '../components/DetalleHome/DetalleHome';
 import { Historia } from '../pages/Historia/Historia';
-import {Tienda } from '../pages/Tienda/Tienda';
 import { Tienda } from "../pages/Tienda/Tienda";
 import { Blog } from "../pages/Blog/Blog";
+import { ShopSingle } from "../pages/ShopSingle/ShopSingle";
 
 export const RouterApp = createBrowserRouter([
   {
@@ -19,15 +19,27 @@ export const RouterApp = createBrowserRouter([
     path: "historia",
     element: <Historia/>,
   },
+ 
 
-  { 
-  path: "tienda",
-  element: <Tienda/>,
-},
 {
   path: "blog",
   element: <Blog/>
 },
 
+
+{
+ path: "tienda",
+ element: <Tienda/>
+},
+
+{
+  path: "detalleHome",
+  element: <DetalleHome/>
+ },
+
+ {
+  path: "shopSingle",
+  element: <ShopSingle/>
+ },
 
 ]);

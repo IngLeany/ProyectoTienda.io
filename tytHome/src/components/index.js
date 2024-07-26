1d3bb85f12553a325ac5b31b3e38482e37837122
IngLeany <leany2902@gmail.com>
2024-07-26 14:15:32 -0500

    Update index.js

--- tytHome/src/components/index.js
+++ tytHome/src/components/index.js
@@ -2,3 +2,4 @@ export { FooterHome } from "./FooterHome/FooterHome";
 export { Historia } from "../pages/Historia/Historia";
 export { Tienda } from "../pages/Tienda/Tienda";
 export { Blog } from "../pages/Blog/Blog";
+export { ShopSingle } from "../pages/ShopSingle/ShopSingle";

13280b207522f7becc40f2463ab649d104bfd04c
IngLeany <leany2902@gmail.com>
2024-07-26 14:10:01 -0500

    Update Barrel.js

--- tytHome/src/Barrel/Barrel.js
+++ tytHome/src/Barrel/Barrel.js
@@ -1,7 +1,66 @@
-export const Barrel = {
-    "Vestido de baño 1": "../assets/images/1.png",
-    "Vestido de baño 2": "../assets/images/2.png",
-    "Vestido de baño 3": "../assets/images/3.png",
+import vestidodebañodospiezas from "../assets/images/vestidodebañodospiezas.png";
+import vestidodebañodospiezacondorado from "../assets/images/vestidodevañodospiezacondorado.png";
+import vestidodebañodospiezasstrapless from "../assets/images/vestidodebañodedospiezasstrapless.png";
+import cuatro from "../assets/images/cuatro.png";
+import cinco from "../assets/images/cinco.png";
+import seis from "../assets/images/seis.png";
+import siete from "../assets/images/siete.png";
+import ocho from "../assets/images/ocho.png";
+import nueve from "../assets/images/nueve.png";
+import diez from "../assets/images/diez.png";
+import once from "../assets/images/once.png";
+import doce from "../assets/images/doce.png";
+import trece from "../assets/images/trece.png";
+import catorce from "../assets/images/catorce.png";
+import quince from "../assets/images/quince.png";
+import diezyseis from "../assets/images/diezyseis.png";
+import diezysiete from "../assets/images/diezysiete.png";
+import diezyocho from "../assets/images/diezyocho.png";
+import diezynueve from "../assets/images/diezynueve.png";
+import veinte from "../assets/images/veinte.png";
+import ventiuno from "../assets/images/ventiuno.png"
+import veintidos from "../assets/images/veintidos.png";
+import pijama from "../assets/images/pijama.png";
+import pijama1 from "../assets/images/pijama1.png";
+import pijama3 from "../assets/images/pijama3.png";
+import batola from "../assets/images/batola.png";
+import kimono from "../assets/images/kimono.png";
+import batola2 from "../assets/images/batola2.png";
+
+
+const imagenes = {
+    vestidodebañodospiezas: vestidodebañodospiezas,
+    vestidodebañodospiezacondorado: vestidodebañodospiezacondorado,
+    vestidodebañodospiezasstrapless: vestidodebañodospiezasstrapless,
+    cuatro: cuatro,
+    cinco: cinco,
+    seis: seis,
+    siete: siete,
+    ocho: ocho,
+    nueve: nueve,
+    diez: diez,
+    once: once,
+    doce: doce,
+    trece: trece,
+    catorce: catorce,
+    quince: quince,
+    diezyseis: diezyseis,
+    diezysiete: diezysiete,
+    diezyocho: diezyocho,
+    diezynueve: diezynueve,
+    veinte: veinte,
+    ventiuno: ventiuno,
+    veintidos: veintidos,
+    pijama: pijama,
+    pijama1: pijama1,
+    pijama3: pijama3,
+    batola: batola,
+    kimono: kimono,
+    batola2: batola2,
+
 };
+
+export default imagenes;
+
 
 
